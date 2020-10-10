c2c930072709f2dbb17a45cdc920fc28ddfc1e46
Erkan Maraş <erkanm@nebim.com.tr>
2020-10-10 14:04:06 +0300

    google map and ios location permissions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,17 @@
 import UIKit
 import Flutter
-
+// Google map
+import GoogleMaps
+// Google map
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      // Google map Api Key
+      GMSServices.provideAPIKey("AIzaSyBI_-xfrCCRa5W7RnQIP7q3gGKiOckpPz8")
+      // Google map Api Key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/ui/pages/track_location_page/track_location_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TrackLocationPage extends StatefulWidget {
+  @override
+  _TrackLocationPageState createState() => _TrackLocationPageState();
+}
+
+class _TrackLocationPageState extends State<TrackLocationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Servis Rota'),
+        actions: <Widget>[],
+      ),
+      body: const Center(
+        
+      ),
+    );
+  }
+}
